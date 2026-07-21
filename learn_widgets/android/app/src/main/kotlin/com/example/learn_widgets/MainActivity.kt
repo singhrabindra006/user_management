5de003e469583829389e6010ebbb4a6800c7a8b6
Rabindra Kumar Singh <singhrabindra9777@gmail.com>
2026-07-21 18:40:06 +0545

    Delete learn_widgets directory

--- learn_widgets/android/app/src/main/kotlin/com/example/learn_widgets/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.learn_widgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- learn_widgets/lib/app_bloc_observer.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class AppBlocObserver extends BlocObserver {
-  @override
-  void onCreate(BlocBase<dynamic> bloc) {
-    super.onCreate(bloc);
-    print(bloc);
-  }
-}
